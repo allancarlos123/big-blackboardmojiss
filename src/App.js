752484fd81f7bc8f0467a3b049730d77aae4e996
Allan <allancarlosdeveloper@gmail.com>
2018-04-06 13:23:10 -0300

    click on the text to copy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { translate } from 'moji-translate'
 import './App.css'
 
 class App extends Component {
   state = {
     // text: 'Qualquer um texto aqui aqui...'
-    text: ''
+    text: '',
+    copied: false
   }
 
   handleChange(event) {
@@ -15,18 +17,26 @@ class App extends Component {
   render() {
     return <div className="App">
         <h1>
-          <span role="img" aria-label="high voltage">⚡️</span> We translate your text to emoji <span role="img" aria-label="hands down">👇</span>
+          <span role="img" aria-label="high voltage">
+            ⚡️
+          </span> We translate your text to emoji <span role="img" aria-label="hands down">
+            👇
+          </span>
         </h1>
 
-        <textarea
-          className="Textarea"
-          type="text"
-          placeholder="#typeSomething🧙‍"
-          onChange={e => this.handleChange(e)}
-          autoFocus
-        />
+        <textarea className="Textarea" type="text" placeholder="#typeSomething🧙‍" onChange={e => this.handleChange(e)} autoFocus />
 
-        {this.state.text && <pre className="Result">{translate(this.state.text)}</pre>}
+        {this.state.text && 
+          <CopyToClipboard text={translate(this.state.text)} onCopy={() => this.setState({ copied: true })}>
+            <pre className="Result">
+              {translate(this.state.text)}
+            </pre>
+          </CopyToClipboard>
+        }
+
+        {this.state.copied ? <span style={{ color: "red" }}>
+            Copied.
+          </span> : null}
       </div>;
   }
 }
