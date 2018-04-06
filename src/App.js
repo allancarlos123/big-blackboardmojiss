7d12147ad26aa9d442f999577d811102c5dbd7c8
Allan <allancarlosdeveloper@gmail.com>
2018-04-06 11:22:18 -0300

    feat: convert to emoji

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { translate } from 'moji-translate'
+import './App.css'
 
 class App extends Component {
+  state = {
+    // text: 'Qualquer um texto aqui aqui...'
+    text: ''
+  }
+
+  handleChange(event) {
+    this.setState({text: event.target.value})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">
+        <h1>
+          <span role="img" aria-label="high voltage">⚡️</span> We translate your text to emoji <span role="img" aria-label="hands down">👇</span>
+        </h1>
+
+        <textarea
+          className="Textarea"
+          type="text"
+          placeholder="#typeSomething🧙‍"
+          onChange={e => this.handleChange(e)}
+          autoFocus
+        />
+
+        {this.state.text && <pre className="Result">{translate(this.state.text)}</pre>}
+      </div>;
   }
 }
 
